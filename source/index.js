8f2288de31b4e279f2166235c09c794af652081e
Yefri González <yefrig00@gmail.com>
2021-12-18 18:38:06 -0600

    Login añadido

--- source/index.js
+++ source/index.js
@@ -3,9 +3,9 @@ const res = require('express/lib/response');
 const app=express();
 const port=process.env.PORT || 5000;
 const path=require('path');
-
+app.use(express.static(__dirname+'/public/'));
 app.get('/',(req,res) =>{
-    res.status(201).sendFile(path.join(__dirname,'../views/html/index.html'));
+    res.status(201).sendFile(path.join(__dirname,'/public/html/index.html'));
 })
 
 app.listen(port,()=>console.log('hola mundo'));

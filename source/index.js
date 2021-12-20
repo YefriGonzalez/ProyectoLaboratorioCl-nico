62de6391d6db9b7a484112a5a4365dc613e2089e
Annelís <juanysacalxot376@gmail.com>
2021-12-19 23:53:07 -0600

    Creación de plantillas en examenLab e IngresoDatos

--- source/index.js
+++ source/index.js
@@ -12,11 +12,19 @@ app.get('/',(req,res) =>{
     res.status(201).sendFile(path.join(__dirname,'/public/html/index.html'));
 });
 
+//Laboratorio
 app.get('/datosLaboratorio' , (req, res) =>{
     res.status(201).sendFile(path.join(__dirname,'/public/html/datosLaboratorio.html'));
 });
 
-//listening server
+app.get('/ingresoDatos' , (req, res) =>{
+    res.status(201).sendFile(path.join(__dirname,'/public/html/ingresoDatos.html'));
+});
+
+app.get('/resultadoExamen' , (req, res) =>{
+    res.status(201).sendFile(path.join(__dirname,'/public/html/examenLab.html'));
+});
+
 app.listen(port,()=>console.log('hola mundo'));
 app.get('/Administracion',(req,res)=>{
     res.status(201).sendFile(path.join(__dirname,'/public/html/administracion.html'));
---
+++ source/public/js/confLab.js
@@ -0,0 +1,5 @@
+function abrirURL(url, contenedor){
+    $.get(url,{},function(data){
+        $("#"+contenedor).html(data);
+    });
+}

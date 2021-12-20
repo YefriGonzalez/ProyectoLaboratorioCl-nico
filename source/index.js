4b72b6c8e0dffcaa973b854d67e762d5d1dc171a
Annelís <juanysacalxot376@gmail.com>
2021-12-20 17:38:17 -0600

    Fusion

--- source/index.js
+++ source/index.js
@@ -12,10 +12,6 @@ app.get('/',(req,res) =>{
     res.status(201).sendFile(path.join(__dirname,'/public/html/index.html'));
 });
 
-app.get('/datosLaboratorio' , (req, res) =>{
-    res.status(201).sendFile(path.join(__dirname,'/public/html/datosLaboratorio.html'));
-});
-
 //listening server
 app.listen(port,()=>console.log('Servidor encendido'));
 app.get('/Administracion',(req,res)=>{
@@ -25,15 +21,18 @@ app.get('/Administracion',(req,res)=>{
 
 //Laboratorio
 app.get('/datosLaboratorio' , (req, res) =>{
-    res.status(201).sendFile(path.join(__dirname,'/public/html/datosLaboratorio.html'));
+    res.status(201).sendFile(path.join(__dirname,'/public/html/Laboratorista/datosLaboratorio.html'));
+    console.log("Prerequisito: Inicio de sesión");
 });
 
 app.get('/ingresoDatos' , (req, res) =>{
-    res.status(201).sendFile(path.join(__dirname,'/public/html/ingresoDatos.html'));
+    res.status(201).sendFile(path.join(__dirname,'/public/html/Laboratorista/ingresoDatos.html'));
+    console.log("Prerequisito: 1.Inicio de sesion 2. LLenar campos 3. Boton guardar paciente")
 });
 
 app.get('/resultadoExamen' , (req, res) =>{
-    res.status(201).sendFile(path.join(__dirname,'/public/html/examenLab.html'));
+    res.status(201).sendFile(path.join(__dirname,'/public/html/Laboratorista/examenLab.html'));
+    console.log("Prerequisito: 1.Inicio de sesion 2. LLenar campos 3. Boton guardar resultados")
 });
 
 //Administración
@@ -47,17 +46,4 @@ app.get('/Secretaria',(req,res)=>{
     res.status(201).sendFile(path.join(__dirname,'/public/html/Secretaria/AreaSecretaria.html'))
     
     console.log("Prerequisito Inicio de sesión")
-});
-
-//Creación endpon para el área de ingreso de datos
-app.get('/Secretaria/IngresarDatos',(req,res)=>{
-    res.status(201).sendFile(path.join(__dirname,'/public/html/Secretaria/IngresarDatos.html'))
-
-    console.log("Prerequisito: 1. A ver ingresado al área de secretaria 2.Boton Ingresar datos del paciente y realizar cobro")
-});
-
-//Creacion endpon para el área imprimir resultados
-app.get('/Secretaria/ImprimirResultados',(req,res)=>{
-    res.status(201).sendFile(path.join(__dirname,'/public/html/Secretaria/ImprimirResultados.html'))
-    console.log("Prerequisito:1.A ver ingresado al área de secretaria 2.Boton Imprimir resultados de paciente")
 });

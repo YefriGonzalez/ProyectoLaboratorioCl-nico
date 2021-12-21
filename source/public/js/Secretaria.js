f4a73288a821ffb5455e36b4ea95d1e5adac7dba
Melanni Tzul <melannitzul@gmail.com>
2021-12-20 20:02:24 -0600

    validacion de datos ingresados en el area de secretaria

--- source/public/js/Secretaria.js
+++ source/public/js/Secretaria.js
@@ -1,6 +1,23 @@
 
-//
+//Funcion que me permite mostrar en consola los datos ingresados
 function IngresoDatosPaciente(){
+    let Nombre=document.getElementById("Nombre:").value;
+    let Edad=document.getElementById("Edad:").value;
+    let Fecha_Nacimiento=document.getElementById("Fecha de nacimiento").value;
+    let Sexo=document.getElementById("Sexo:").value;
+    let Direccion=document.getElementById("Dirección:").value;
+    let Afiliacion=document.getElementById("Afiliación No:").value;
+    let Telefono=document.getElementById("Teléfono:").value;
+    let Correo=document.getElementById("Correo:").value;
+    console.log(Nombre);
+    console.log(Edad);
+    console.log(Fecha_Nacimiento);
+    console.log(Sexo);
+    console.log(Direccion);
+    console.log(Afiliacion);
+    console.log(Telefono);
+    console.log(Correo);
+    alert("¡Datos guardados exitosamente!");
 
 };
 //
@@ -21,3 +38,5 @@ function ImprimirResultados(){
 };
 
 function ModificarDatos(){};
+
+

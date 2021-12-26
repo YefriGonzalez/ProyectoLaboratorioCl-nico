0169dd879acef1c75ee26c874bad5b8f819aa9d9
Annelís <juanysacalxot376@gmail.com>
2021-12-26 02:31:22 -0600

    Laboratorista: Modificacion en ingresoDatos y creacion de metodo para las tablas de los examenes(aun no redirige)

--- source/public/js/confLab.js
+++ source/public/js/confLab.js
@@ -1,12 +1,43 @@
-function abrirURL(url, contenedor){
-    $.get(url,{},function(data){
-        $("#"+contenedor).html(data);
-    });
+//Para mostrar el formulario dependiendo el tipo de examen
+
+var hematologia = document.getElementById("examenHematologia").value;
+var orina = document.getElementById("examenOrina").value;
+var heces = document.getElementById("examenHeces").value;
+var completo = document.getElementById("examenHematologia, examenOrina, examenHeces").value;
+
+function tipoExamen(){
+    if (hematologia == exHematologia()){
+        console.log(hematologia);
+    } else if (orina == exOrina()){
+        console.log(orina);
+    } else if(heces == exHeces()){
+        console.log(heces);
+    } else if (completo == exCompleto()){
+        console.log(completo);
+    }
 }
+
+function exHematologia(){
+    console.log(hematologia);
+}
+
+function exOrina(){
+    console.log(orina);
+}
+
+function exHeces(){
+    console.log(heces);
+}
+
+function exCompleto(){
+    console.log(completo);
+}
+
+
 
 //Mostrando los datos en consola
 //ingreso de paciente
-function mostrarDatosLaboratorio(){
+/*function mostrarDatosLaboratorio(){
 let No_Orden=document.getElementById("Orden").value;
 let No_Afiliacion=document.getElementById("Afiliacion").value;
 let Fecha_Ingreso=document.getElementById("FechaIngreso").value;
@@ -32,4 +63,4 @@ console.log(Telefono);
 console.log(Referido_Por);
 console.log(Codigo);
 alert("Datos guardados con éxito");
-}
+}*/

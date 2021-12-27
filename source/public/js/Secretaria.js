167ba0785702dddaf69a1689b281007bce725df4
Melanni Tzul <melannitzul@gmail.com>
2021-12-27 03:38:19 -0600

    actualizando

--- source/public/js/Secretaria.js
+++ source/public/js/Secretaria.js
@@ -1,24 +1,11 @@
 
 //Funcion que me permite mostrar en consola los datos ingresados
 function IngresoDatosPaciente(){
-   
-    const Nombre=document.getElementById("nombre").value;
-    const Edad=document.getElementById("edad").value;
-    const Fecha_Nacimiento=document.getElementById("fechaNacimiento").value;
-    const Sexo=document.getElementById("sexo").value;
-    const Direccion=document.getElementById("direccion").value;
-    const Afiliacion=document.getElementById("afiliacion").value;
-    const Telefono=document.getElementById("telefono").value;
-    const Correo=document.getElementById("correo").value;
-    console.log(Nombre);
-    console.log(Edad);
-    console.log(Fecha_Nacimiento);
-    console.log(Sexo);
-    console.log(Direccion);
-    console.log(Afiliacion);
-    console.log(Telefono);
-    console.log(Correo);
-    alert("¡Datos guardados exitosamente!");
+    try{location.href="/Secretaria/IngresarDatos"
+
+    }catch(error){
+
+    }
 
 }
 //
